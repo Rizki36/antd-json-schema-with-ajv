0ee731d65c1281aec81eecb00db664a04a87e7d7
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-01-29 20:38:56 +0700

    feat: create useJsonSchema

--- src/hooks/useJsonSchema.ts
+++ src/hooks/useJsonSchema.ts
@@ -1,7 +1,171 @@
-import type { JTDSchemaType } from 'ajv/dist/core';
+import type { JSONSchemaType, ValidateFunction } from 'ajv';
+import Ajv from 'ajv';
+import AjvErrors from 'ajv-errors';
+import type { RuleObject } from 'antd/es/form';
+import type { StoreValue } from 'antd/es/form/interface';
+import { useMemo } from 'react';
 
-const useJsonSchema = <T>(schema: JTDSchemaType<T>) => {
-  console.log(schema);
+const ajv = new Ajv({
+  strict: false,
+  allErrors: true,
+});
+
+AjvErrors(ajv);
+
+type UseJsonSchemaOptions = {
+  mapFields: Record<string, string>;
+};
+
+type ValidateOptions<D> = {
+  onSuccess?: (data: D, validator: ValidateFunction<D>) => void;
+  onError?: (
+    errors: ValidateFunction['errors'],
+    validator: ValidateFunction<D>
+  ) => void;
+};
+
+/**
+ * D - the type of the data to be validated
+ * SFN - the type of the schema field name
+ */
+function useJsonSchema<D = unknown, SFN = string>(
+  schema: JSONSchemaType<D>,
+  options: UseJsonSchemaOptions
+) {
+  const validator = useMemo(() => {
+    const finalSchema = {
+      ...schema,
+    };
+    return ajv.compile<D>(finalSchema);
+  }, [ajv, schema]);
+
+  /**
+   * validate all data, to make sure that all fields are valid
+   */
+  const validate = (data: unknown, validateOptions?: ValidateOptions<D>) => {
+    if (validator(data)) {
+      validateOptions?.onSuccess?.(data as D, validator);
+    } else {
+      validateOptions?.onError?.(validator.errors, validator);
+    }
+  };
+
+  /**
+   * validate a single field
+   */
+  const validateField =
+    (SchemaFieldName: SFN) => (rule: RuleObject, value: StoreValue) => {
+      // @ts-ignore
+      const localField = rule?.field;
+
+      const data = {
+        [`${SchemaFieldName}`]: value,
+      };
+
+      if (validator(data)) {
+        return Promise.resolve();
+      }
+
+      const errorsFields = errorsToFields(validator.errors, {
+        [`${SchemaFieldName}`]: localField,
+      });
+
+      const currentFieldError = errorsFields.find(
+        (error) => error.name === localField
+      );
+
+      if (!currentFieldError) return Promise.resolve();
+
+      if (currentFieldError.errors?.[0]) {
+        return Promise.reject(currentFieldError.errors[0]);
+      }
+
+      return Promise.resolve();
+    };
+
+  /**
+   * for set fields errors using form.setFields
+   */
+  const mapErrorsToFields = () => {
+    return errorsToFields(validator.errors, options.mapFields);
+  };
+
+  return { validate, validateField, mapErrorsToFields };
+}
+
+/**
+ * mapper - a map of schema field name to local field name
+ */
+const errorsToFields = (
+  errors: ValidateFunction['errors'],
+  mapper: Record<string, string> // { schemaFieldName: 'localFieldName' }
+) => {
+  if (!errors || !mapper) return [];
+
+  const fields: { name: string; errors: string[] }[] = [];
+
+  errors?.forEach((error) => {
+    // handle errorMessage from ajv-errors
+    if (error.keyword === 'errorMessage') {
+      if (error.params.errors) {
+        error.params.errors.forEach((err: any) => {
+          if (err.keyword === 'required') {
+            const fieldName = mapper[err.params.missingProperty];
+            if (error?.message && fieldName) {
+              fields.push({
+                name: fieldName,
+                errors: [error.message],
+              });
+            }
+          }
+          if (err.instancePath) {
+            const fieldName = getFieldName(err.instancePath, mapper);
+            if (error.message && fieldName) {
+              fields.push({
+                name: fieldName,
+                errors: [error.message],
+              });
+            }
+          }
+        });
+      }
+      return;
+    }
+
+    // handle errorMessage from default ajv
+    if (error.keyword === 'required') {
+      if (error.params.missingProperty) {
+        const fieldName = mapper[error.params.missingProperty];
+        if (error?.message && fieldName) {
+          fields.push({
+            name: fieldName,
+            errors: [error.message],
+          });
+        }
+      }
+    }
+    if (error.instancePath) {
+      const fieldName = getFieldName(error.instancePath, mapper);
+
+      if (error.message && fieldName) {
+        fields.push({
+          name: fieldName,
+          errors: [error.message],
+        });
+      }
+    }
+  });
+
+  return fields;
+};
+
+// TODO: handle nested fields
+const getFieldName = (path: string, mapper: Record<string, string>) => {
+  const pathParts = path.split('/');
+  const fieldName = pathParts[pathParts.length - 1];
+  if (!fieldName) return null;
+
+  return mapper[fieldName];
 };
 
 export default useJsonSchema;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,176 @@
+import type { JSONSchemaType } from 'ajv';
+import { Button, Checkbox, Form, Input, InputNumber } from 'antd';
+
+import useJsonSchema from '@/hooks/useJsonSchema';
+
+type SchemaData = {
+  username: string;
+  password: string;
+  phone: string;
+  age?: number;
+  acceptTerms: boolean;
+};
+
+// prefix local just for example when field name is different with json schema
+type LocalData = {
+  localUsername: string;
+  localPassword: string;
+  localPhone: string;
+  localAge: number;
+  localAcceptTerms: boolean;
+};
+
 const Index = () => {
-  return <>test</>;
+  const [form] = Form.useForm<LocalData>();
+
+  // TODO: get from api
+  const jsonSchema: JSONSchemaType<SchemaData> = {
+    type: 'object',
+    properties: {
+      username: {
+        type: 'string',
+        nullable: false,
+        minLength: 3,
+      },
+      password: {
+        type: 'string',
+        nullable: false,
+        minLength: 6,
+        maxLength: 10,
+      },
+      phone: {
+        type: 'string',
+        nullable: false,
+        pattern: '^((\\+62-?)|0)?[0-9]{10,13}$',
+      },
+      age: {
+        type: 'number',
+        nullable: true, // TODO: handle this case, currently if field is optional, must set nullable to true
+        minimum: 18,
+        maximum: 100,
+      },
+      acceptTerms: {
+        type: 'boolean',
+        nullable: false,
+        const: true,
+        // errorMessage is not official from json schema, but ajv support this
+        errorMessage: {
+          const: 'You must accept terms and conditions',
+        },
+      },
+    },
+    required: ['username', 'password', 'phone'],
+  };
+
+  const { validate, validateField, mapErrorsToFields } = useJsonSchema<
+    SchemaData,
+    keyof SchemaData
+  >(jsonSchema, {
+    mapFields: {
+      username: 'localUsername',
+      password: 'localPassword',
+      phone: 'localPhone',
+      age: 'localAge',
+    },
+  });
+
+  const onFinish = (values: any) => {
+    const formattedValues = {
+      username: values?.localUsername,
+      password: values?.localPassword,
+      passwordConfirmation: values?.localPasswordConfirmation,
+    };
+
+    validate(formattedValues, {
+      onSuccess: (data) => {
+        console.log(data);
+      },
+      onError: () => {
+        const fields = mapErrorsToFields();
+        form.setFields(fields); // set error to form
+      },
+    });
+  };
+
+  return (
+    <div className="p-9">
+      <Form
+        form={form}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Username"
+          name="localUsername"
+          rules={[
+            {
+              validator: validateField('username'),
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="localPassword"
+          rules={[
+            {
+              validator: validateField('password'),
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="phone with regex"
+          name="localPhone"
+          rules={[
+            {
+              validator: validateField('phone'),
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="age (optional)"
+          name="localAge"
+          rules={[
+            {
+              validator: validateField('age'),
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+
+        <Form.Item
+          name="localAcceptTerms"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+          rules={[
+            {
+              validator: validateField('acceptTerms'),
+            },
+          ]}
+        >
+          <Checkbox>Accept term</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="default" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default Index;
